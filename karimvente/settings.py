52c627d6b6a6a20f8d76dfc38b83211b65b9de65
Hassane <imthassane@gmail.com>
2018-09-16 16:17:33 +0200

    Configuration de AWS

--- karimvente/settings.py
+++ karimvente/settings.py
@@ -145,7 +145,6 @@ if DEBUG is False:
     AWS_LOCATION = 'static'
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'karimvente.storages.MediaStorage'
 
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
