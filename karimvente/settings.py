ae107449e101aef00e3d1b2d9ea3105cbe1f0180
Hassane <imthassane@gmail.com>
2018-09-16 15:48:24 +0200

    Configuration de AWS

--- karimvente/settings.py
+++ karimvente/settings.py
@@ -138,7 +138,7 @@ if DEBUG is False:
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_S3_CUSTOM_DOMAIN = 's3.eu-west-3.amazonaws.com/%s%' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = 's3.eu-west-3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
